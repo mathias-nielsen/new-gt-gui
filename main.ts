5ae51a0d7665bb88d2cf5a33a0727e1eedb225f4
Mathias Nielsen <manie449@gmail.com>
2026-05-07 11:06:22 +0200

    basic electron stuff

---
+++ main.ts
@@ -0,0 +1,36 @@
+import { app, BrowserWindow } from 'electron'
+import path from 'path'
+import { registerGtIpc } from './engine/gastown-integration'
+
+const VITE_DEV_SERVER_URL = process.env.VITE_DEV_SERVER_URL
+
+const createWindow = () => {
+    const window = new BrowserWindow({
+        width: 900,
+        height: 650,
+        webPreferences: {
+            preload: path.join(__dirname, 'engine', 'preload.js'),
+            contextIsolation: true,
+            nodeIntegration: false
+        }
+    })
+
+    if (VITE_DEV_SERVER_URL) {
+        window.loadURL(VITE_DEV_SERVER_URL)
+    } else {
+        window.loadFile(path.join(__dirname, '..', 'dist', 'index.html'))
+    }
+    registerGtIpc(window)
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
