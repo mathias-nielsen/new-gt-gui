f04878b80eb88d46dc25ccbb86c6a13183488c48
Mathias Nielsen <manie449@gmail.com>
2026-05-23 14:59:50 +0200

    forgot the Storage index

--- backend/storage/index.ts
+++ backend/storage/index.ts
@@ -1,9 +1,10 @@
+import { CHANNELS } from "../channels";
 import { read, write } from "./storage-integration";
 import { ipcMain } from "electron";
 
 export const registerStorageIPC = () => {
-    ipcMain.handle("storage:get", (_event, key: string) => read()[key] ?? null);
-    ipcMain.handle("storage:set", (_event, key: string, value: unknown) => {
+    ipcMain.handle(CHANNELS.STORAGE.GET, (_event, key: string) => read()[key] ?? null);
+    ipcMain.handle(CHANNELS.STORAGE.SET, (_event, key: string, value: unknown) => {
         write({ ...read(), [key]: value });
     });
 };
