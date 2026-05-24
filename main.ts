1d7a5517f5fb193c51ad40cf84d16b112d0a5f74
Mathias Nielsen <manie449@gmail.com>
2026-05-24 17:13:24 +0200

    Fixing build errors

--- main.ts
+++ main.ts
@@ -14,6 +14,7 @@ const createWindow = () => {
             preload: path.join(__dirname, "preload.js"),
             contextIsolation: true,
             nodeIntegration: false,
+            sandbox: false,
         },
     });
 
